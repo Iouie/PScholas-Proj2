f0378142f7d7a9a67f56ba6351062489d06c7d8f
Louie <sxl7953@rit.edu>
2023-04-18 17:16:11 -0400

    just need to fix a button bug and games is done

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
 const Inputs = (props) => {
+  useEffect(() => {
+    // set number of pages based off filteredData length / 20 and round it up
+    props.setnumPages(Math.ceil(props.filteredData.length / 20));
+  });
+
+  console.log(props.numPages);
   return (
     <div className="eight-bit">
       <li className="hov">
@@ -34,7 +42,16 @@ const Inputs = (props) => {
           </li>
         </ul>
       </li>
-      <input type="text" maxLength="20" placeholder="Enter Game Here" />
+      <input
+        type="text"
+        maxLength="20"
+        placeholder="Enter Game Here"
+        value={props.searchTerm}
+        onChange={(e) => {
+          props.setSearchTerm(e.target.value); // change search term to whatever I type
+          props.setCurrentPage(1); // set current page to 1 everytime I type
+        }}
+      />
     </div>
   );
 };
--- src/pages/Games.js
+++ src/pages/Games.js
@@ -4,13 +4,22 @@ import Inputs from "../components/Inputs";
 const Games = (props) => {
   const itemsPerPage = 20;
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [numPages, setnumPages] = useState(19);
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   useEffect(() => {
     props.getUrl("games"); // change url to games so api fetches game
+    setSearchTerm("");
   }, []);
 
-  const currentData = props.data.slice(startIndex, endIndex); // hold only 20 per page
+  const filteredData = props.data.filter(
+    (
+      item // filter game based off what you type
+    ) => item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const currentData = filteredData.slice(startIndex, endIndex); // hold only 20 games per page
 
   const handleNextPage = () => {
     // put in button to click on next page
@@ -22,36 +31,45 @@ const Games = (props) => {
     setCurrentPage(currentPage - 1);
   };
 
+  const allGames = currentData.map((eachGame) => {
+    // map through each of the list and creates this
+    return (
+      <div className="gameContainer" key={eachGame.id}>
+        <h2>{eachGame.title}</h2>
+        <img
+          src={eachGame.thumbnail}
+          className="gamethumbnail"
+          alt={eachGame.title}
+        />
+        <p className="gamedescription">
+          <span>Description:</span> {eachGame.short_description}
+        </p>
+        <p className="gamegenre">
+          <span>Genre:</span> {eachGame.genre}
+        </p>
+        <p className="gameplatform">
+          <span>Platform:</span> {eachGame.platform}
+        </p>
+        <p className="gamerelease">
+          <span>Release Date:</span> {eachGame.release_date}
+        </p>
+      </div>
+    );
+  });
   return (
     <>
-      <Inputs data={props.data} getParams={props.getParams} />
+      <Inputs
+        getParams={props.getParams}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        numPages={numPages}
+        filteredData={filteredData}
+        setnumPages={setnumPages}
+        setCurrentPage={setCurrentPage}
+      />
       <p className="pagenum">Current Page: {currentPage}</p>
       <div className="maingamecontainer">
-        {currentData.map((eachGame) => {
-          // map through each of the list and creates this
-          return (
-            <div className="gameContainer" key={eachGame.id}>
-              <h2>{eachGame.title}</h2>
-              <img
-                src={eachGame.thumbnail}
-                className="gamethumbnail"
-                alt={eachGame.title}
-              />
-              <p className="gamedescription">
-                <span>Description:</span> {eachGame.short_description}
-              </p>
-              <p className="gamegenre">
-                <span>Genre:</span> {eachGame.genre}
-              </p>
-              <p className="gameplatform">
-                <span>Platform:</span> {eachGame.platform}
-              </p>
-              <p className="gamerelease">
-                <span>Release Date:</span> {eachGame.release_date}
-              </p>
-            </div>
-          );
-        })}
+        {allGames.length !== 0 ? allGames : `No Games Found`}
       </div>
       <div className="allbutton">
         <button
@@ -63,7 +81,7 @@ const Games = (props) => {
         </button>
         <button
           onClick={handleNextPage}
-          disabled={currentPage === 19}
+          disabled={currentPage === numPages}
           className="previousbutton"
         >
           {currentPage + 1}
