08268f2ce14c43d1df5c6b2f4f3004ef5492c32b
Louie <sxl7953@rit.edu>
2023-09-07 01:31:06 -0400

    made the games section responsive

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Dropdown } from "flowbite-react";
 
 const Inputs = (props) => {
   useEffect(() => {
@@ -7,132 +8,176 @@ const Inputs = (props) => {
   }, [props]);
 
   return (
-    <div className="eight-bit">
-      <li className="hov">
-        Sort By
-        <ul className="main">
+    <div className="flex justify-center my-2 gap-x-2 flex-col items-center gap-y-2 md:flex-row md:gap-y-0">
+      <Dropdown label="Sort By" className="cursor-pointer">
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ "sort-by": "relevance" });
             }}
           >
             Relevance
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ "sort-by": "alphabetical" });
             }}
           >
             Alphabetical
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ "sort-by": "release-date" });
             }}
           >
             Release-Date
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ "sort-by": "popularity" });
             }}
           >
             Popularity
           </li>
-        </ul>
-      </li>
-      <li className="hov">
-        Genres
-        <ul className="main">
+        </Dropdown.Header>
+      </Dropdown>
+
+      <Dropdown label="Genres" className="cursor-pointer">
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "mmorpg" });
             }}
           >
             MMORPG
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "mmo" });
             }}
           >
             MMO
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "fantasy" });
             }}
           >
             Fantasy
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "tower-defense" });
             }}
           >
             Tower-Defense
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "shooter" });
             }}
           >
             Shooter
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "moba" });
             }}
           >
             MOBA
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "racing" });
             }}
           >
             Racing
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "sports" });
             }}
           >
             Sports
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "sandbox" });
             }}
           >
             Sandbox
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "open-world" });
             }}
           >
             Open-World
           </li>
+        </Dropdown.Header>
+        <Dropdown.Header>
           <li
+            className="hover:bg-sky-700"
             onClick={() => {
               props.getParams({ category: "battle-royale" });
             }}
           >
             Battle-Royale
           </li>
-        </ul>
-      </li>
-      <input
-        type="text"
-        maxLength="20"
-        placeholder="Enter Game Title Here"
-        value={props.searchTerm}
-        onChange={(e) => {
-          props.setSearchTerm(e.target.value); // change search term to whatever I type
-          props.setCurrentPage(1); // set current page to 1 everytime I type
-        }}
-      />
+        </Dropdown.Header>
+      </Dropdown>
+
+      <div className="sm:w-1/3 text-center">
+        <input
+          type="text"
+          maxLength={20}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Enter Game Title Here"
+          value={props.searchTerm}
+          onChange={(e) => {
+            props.setSearchTerm(e.target.value); // change search term to whatever I type
+            props.setCurrentPage(1); // set current page to 1 everytime I type
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Games.js
+++ src/pages/Games.js
@@ -37,29 +37,29 @@ const Games = (props) => {
     // map through each of the list and creates this
     return (
       <div
-        className="gameContainer"
+        className="flex flex-col items-center mx-auto p-4 gap-x-4 my-2 md:w-1/3 border-dotted border-2 border-sky-500 hover:bg-sky-700 cursor-pointer w-full"
         key={eachGame.id}
         onClick={() => {
           // navigate to GameItem component
           navigate(`/games/${eachGame.id}`);
         }}
       >
-        <h2>{eachGame.title}</h2>
+        <p className="md:text-3xl underline py-2 text-sm">{eachGame.title}</p>
         <img
           src={eachGame.thumbnail}
           className="gamethumbnail"
           alt={eachGame.title}
         />
-        <p className="gamedescription">
+        <p className="md:text-base text-center text-white text-[0.6rem] my-1">
           <span>Description:</span> {eachGame.short_description}
         </p>
-        <p className="gamegenre">
+        <p className="md:text-base text-center text-white text-[0.6rem] my-1">
           <span>Genre:</span> {eachGame.genre}
         </p>
-        <p className="gameplatform">
+        <p className="md:text-base text-center text-white text-[0.6rem] my-1">
           <span>Platform:</span> {eachGame.platform}
         </p>
-        <p className="gamerelease">
+        <p className="md:text-base text-center text-white text-[0.6rem] my-1">
           <span>Release Date:</span> {eachGame.release_date}
         </p>
       </div>
@@ -76,22 +76,22 @@ const Games = (props) => {
         setnumPages={setnumPages}
         setCurrentPage={setCurrentPage}
       />
-      <p className="pagenum">Current Page: {currentPage}</p>
-      <div className="maingamecontainer">
+      <p className="text-[#93deff] text-center">Current Page: {currentPage}</p>
+      <div className="flex flex-wrap">
         {allGames.length !== 0 ? allGames : `No Games Found`}
       </div>
-      <div className="allbutton">
+      <div className="fixed bottom-0 w-full">
         <button
           onClick={handlePreviousPage}
           disabled={currentPage === 1}
-          className="nextbutton"
+          className="my-8 mx-10 px-5 py-2 bg-[#0e7490] text-white text-sm font-bold tracking-wide rounded-full z-50 md:w-[10rem] w-[5rem] hover:bg-opacity-50 cursor-pointer"
         >
           {currentPage}
         </button>
         <button
           onClick={handleNextPage}
           disabled={currentPage >= numPages}
-          className="previousbutton"
+          className="bottom-0 my-8 px-5 py-2 bg-[#0e7490]  text-white text-sm font-bold tracking-wide rounded-full z-50 md:w-[10rem] w-[5rem] float-right mx-10 cursor-pointer hover:bg-opacity-50"
         >
           {currentPage + 1}
         </button>
